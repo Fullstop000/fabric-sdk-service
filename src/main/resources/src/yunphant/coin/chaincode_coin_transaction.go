d13cbf9924dbbb729fae4d49e328536171aebabe
Hetian Zhu <zhuht@yunphant.com>
2017-09-18 21:00:01 +0800

    [refactor] Update chaincode_coin_transaction.go : Move the balance calculation into chaincode

--- src/main/resources/src/yunphant/coin/chaincode_coin_transaction.go
+++ src/main/resources/src/yunphant/coin/chaincode_coin_transaction.go
@@ -14,12 +14,8 @@ type CoinTransaction struct {
 	Sender int `json:"sender"`
 	// transaction receiver
 	Receiver int `json:"receiver"`
-	// sender balance
-	SenderBalance float64 `json:"senderBalance"`
-	// receiver balance
-	ReceiverBalance float64 `json:"receiverBalance"`
 	// transaction amount
-	Amount float64 `json:"amount"`
+	Amount int64 `json:"amount"`
 	// transaction type
 	Type string `json:"type"`
 	// transaction time
@@ -28,32 +24,45 @@ type CoinTransaction struct {
 	//todo add goods info
 }
 
-func (coinTransaction *CoinTransaction) Init(stub shim.ChaincodeStubInterface) peer.Response {
+type UserBalance struct {
+	// user id
+	UserId int `json:"userId"`
+
+	// user balance
+	Balance int64 `json:"balance"`
+}
+
+type YunphantCoinCC struct {
+// 	just for chaincode
+}
+func (c *YunphantCoinCC) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	args := stub.GetArgs()
 	fmt.Printf("Starting init . Args : %v", args)
 	return shim.Success(nil)
 }
 
-func (coinTransaction *CoinTransaction) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+func (c *YunphantCoinCC) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	function, args := stub.GetFunctionAndParameters()
 	switch function {
-	//case "query":
-		//return coinTransaction.query(stub, args)
+	case "createAccount":
+		return c.createAccount(stub, args[0])
 	case "addCoinTransaction":
 		var coinTran CoinTransaction
 		err := json.Unmarshal([]byte(args[0]), &coinTran)
 		if err != nil {
 			shim.Error(fmt.Sprintf("Erroring unmarshal string to CoinTransaction : %s ,err : %e",args[0],err))
 		}
-		return coinTran.addCoinTransaction(stub)
+		return c.addCoinTransaction(stub,&coinTran)
 	case "query":
-		return  coinTransaction.query(stub,args[0])
+		return  c.query(stub,args[0])
 	default:
 		break
 	}
 	return shim.Success([]byte(fmt.Sprintf("Invalid invoke function : %s",function)))
 }
-func (coinTransaction *CoinTransaction) query(stub shim.ChaincodeStubInterface, queryStr string ) peer.Response {
+
+
+func (c *YunphantCoinCC) query(stub shim.ChaincodeStubInterface, queryStr string ) peer.Response {
 	res, err := stub.GetQueryResult(queryStr)
 	if err != nil {
 		shim.Error(fmt.Sprintf("Erroring query couchDB by query string : %s , err : %e",queryStr , err))
@@ -79,24 +88,85 @@ func (coinTransaction *CoinTransaction) query(stub shim.ChaincodeStubInterface, 
 	return shim.Success(buffer.Bytes())
 }
 
-func (coinTransaction *CoinTransaction) addCoinTransaction(stub shim.ChaincodeStubInterface) peer.Response {
+// createAccount
+// this func will create a new pair <userID , 0> and put it into couchDB
+func (c *YunphantCoinCC) createAccount(stub shim.ChaincodeStubInterface , userId string) peer.Response {
+	res , err := stub.GetState(userId)
+	if err != nil  {
+		shim.Error(fmt.Sprintf("Error check user's balance by userID : %s , err : %s",userId,err.Error()))
+	}
+	if res != nil {
+		shim.Error(fmt.Sprint("The user has already created"))
+	}
+	err = stub.PutState(userId,[]byte(strconv.Itoa(0)))
+	if err != nil {
+		shim.Error(fmt.Sprintf("Error putting new userId into DB : %s",err.Error()))
+	}
+	return shim.Success([]byte("Success create Account for user : " + userId))
+}
+
+// addCoinTransaction
+// this func will update both receiver's and sender's balance and create a transaction record
+func (c *YunphantCoinCC) addCoinTransaction(stub shim.ChaincodeStubInterface,coinTransaction *CoinTransaction) peer.Response {
+	senderId := strconv.Itoa(coinTransaction.Sender)
+	receiverId := strconv.Itoa(coinTransaction.Receiver)
+	amount := coinTransaction.Amount
+
+	// update the sender balance and calculate
+	senderBalanceBytes,err := stub.GetState(senderId)
+	if err !=nil {
+		shim.Error(fmt.Sprintf("Error getting sender balance by id : %s , err : %s",senderId,err.Error()))
+	}
+	senderBalance, err := strconv.ParseInt(string(senderBalanceBytes[:]),10,64)
+	if err != nil {
+		shim.Error(fmt.Sprintf("Error parsing sender balance to int64 number : %s",err.Error()))
+	}
+
+	if senderBalance=senderBalance - amount ; senderBalance < 0  {
+		shim.Error(fmt.Sprint("Do not have enough balance !"))
+	}
+	err = stub.PutState(senderId, []byte(strconv.FormatInt(senderBalance,10)))
+	if err != nil {
+		shim.Error(fmt.Sprintf("Error update sender balance : %s",err.Error()))
+	}
+
+	// update the receiver balance
+	receiverBalanceBytes, err := stub.GetState(receiverId)
+	if err!= nil {
+		shim.Error(fmt.Sprintf("Error getting receiver balance by id : %s , err : %s",senderId,err.Error()))
+	}
+	receiverBalance, err := strconv.ParseInt(string(receiverBalanceBytes[:]),10,64)
+	if err != nil {
+		shim.Error(fmt.Sprintf("Error parsing receiver balance to int64 number : %s",err.Error()))
+	}
+	receiverBalance += amount
+	err = stub.PutState(receiverId,[]byte(receiverBalance))
+	if err != nil {
+		shim.Error(fmt.Sprintf("Error update receiver balance : %s",err.Error()))
+	}
+
+
+	// create composite key for transaction record
 	key, err := stub.CreateCompositeKey(coinTransaction.Type,
 		[]string{strconv.Itoa(coinTransaction.Sender), strconv.Itoa(coinTransaction.Receiver), strconv.FormatInt(coinTransaction.Timestamp, 10)})
 	if err != nil {
-		shim.Error(fmt.Sprintf("Error generate composite key from CoinTransaction : %v , err : %e", coinTransaction, err))
+		shim.Error(fmt.Sprintf("Error generating composite key from CoinTransaction : %v , err : %s", c, err.Error()))
 	}
-	jsonByte, err := json.Marshal(coinTransaction)
+	jsonByte, err := json.Marshal(c)
 	if err != nil {
-		shim.Error(fmt.Sprintf("Error convert 'CoinTransaction' to json :  %v , err : %e", coinTransaction, err))
+		shim.Error(fmt.Sprintf("Error converting 'CoinTransaction' to json :  %v , err : %s", c, err.Error()))
 	}
-	stub.PutState(key, jsonByte)
+	err = stub.PutState(key, jsonByte)
+	if err != nil {
+		shim.Error(fmt.Sprintf("Error create transaction record : %s",err.Error()))
+	}
 	return shim.Success([]byte("Success adding coin transaction"))
 }
 
 
 func main() {
-	err := shim.Start(new(CoinTransaction))
+	err := shim.Start(new(YunphantCoinCC))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error starting Simple chaincode: %s", err.Error())
 	}
 }
